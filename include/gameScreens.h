c3597d84a2841d4bda069c57fbc0c85d2624c21c
amurph <aidanpmurphy@hotmail.com>
2024-06-13 17:31:54 -0700

    remove unecessary funcs in header file

--- include/gameScreens.h
+++ include/gameScreens.h
@@ -1,12 +1,6 @@
 #ifndef GAMESCREENS_H
 #define GAMESCREENS_H
 
-void play_round(int* player_score, int* computer_score);
-
-int show_rules();
-
-int reset_scores(int* score1, int* score2);
-
 int enter_menu(int* player_score, int* computer_score);
 
 #endif
